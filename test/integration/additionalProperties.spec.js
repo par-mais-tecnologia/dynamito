9976b470fe226c2e5c79c987a614569b0961758f
diego.laucsen <diego.laucsen@gmail.com>
2017-09-27 14:23:19 -0300

    Adding get to multiple fields.

--- test/integration/additionalProperties.spec.js
+++ test/integration/additionalProperties.spec.js
@@ -29,18 +29,89 @@ describe('Additional Properties Option: ', () => {
 
   beforeEach(() => genSample());
 
-  it('should accept additional fields when configured', () => {
-    return sample.save().then(saved => {
-      const item = saved.toJSON();
-      expect(item).to.deep.equal({
-        email: 'some.email@target.com',
-        name: 'Derpy',
-        last: 'Lancotre!',
-        age: 42,
-        password: '123456789',
-        someAdditionalField: 'Additional Data',
-        someExtraAdditionalField: 'Extra Data',
-      });
+  describe('Saving: ', () => {
+    afterEach(() => {
+      return PersonModel
+        .scan()
+        .exec()
+        .then(data => {
+          expect(data.length).to.be.equal(1);
+          expect(data[0].toJSON()).to.deep.equal({
+            email: 'some.email@target.com',
+            name: 'Derpy',
+            last: 'Lancotre',
+            age: 42,
+            password: '123456789',
+            someAdditionalField: 'Additional Data',
+            someExtraAdditionalField: 'Extra Data',
+          });
+          return null;
+        });
+    });
+
+    it('should save additional fields when configured - PUT', () => {
+      return sample.save();
+    });
+  });
+
+  describe('Getting: ', () => {
+    beforeEach(() => sample.save());
+
+    it('should get additional fields on SCAN', () => {
+      return PersonModel
+        .scan()
+        .exec()
+        .then(data => {
+          expect(data.length).to.be.equal(1);
+          expect(data[0].toJSON()).to.deep.equal({
+            email: 'some.email@target.com',
+            name: 'Derpy',
+            last: 'Lancotre',
+            age: 42,
+            password: '123456789',
+            someAdditionalField: 'Additional Data',
+            someExtraAdditionalField: 'Extra Data',
+          });
+          return null;
+        });
+    });
+
+    it('should get additional fields on GET', () => {
+      return PersonModel
+        .findById('some.email@target.com')
+        .exec()
+        .then(data => {
+          expect(data.toJSON()).to.deep.equal({
+            email: 'some.email@target.com',
+            name: 'Derpy',
+            last: 'Lancotre',
+            age: 42,
+            password: '123456789',
+            someAdditionalField: 'Additional Data',
+            someExtraAdditionalField: 'Extra Data',
+          });
+          return null;
+        });
+    });
+
+    it('should get additional fields on QUERY', () => {
+      return PersonModel
+        .query()
+        .find(new PersonModel.Queries.Equal('email', 'some.email@target.com'))
+        .exec()
+        .then(data => {
+          expect(data.length).to.be.equal(1);
+          expect(data[0].toJSON()).to.deep.equal({
+            email: 'some.email@target.com',
+            name: 'Derpy',
+            last: 'Lancotre',
+            age: 42,
+            password: '123456789',
+            someAdditionalField: 'Additional Data',
+            someExtraAdditionalField: 'Extra Data',
+          });
+          return null;
+        });
     });
   });
 });
